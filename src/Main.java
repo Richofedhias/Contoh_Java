9b47652d9fe1566de86d838a41745b7abf8f09fc
Richo Fedhia <fedhia64@gmail.com>
2020-09-08 21:00:44 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Selamat Datang Di applikasi");
+        System.out.println("ADI\nWicaksono");
+        System.out.println("ADI Wicaksono");
+        System.out.println("selamat datang di aplikasi".replaceAll("a","i"));
+        int a = 10;
+        int b = 5;
+        float as = 10;
+        float bd = 3;
+        System.out.println(as/bd);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+//
+
+    }
+}
